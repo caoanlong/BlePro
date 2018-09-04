551f79a6045cd19aca8ec76a94d383582932a778
caoanlong <476977540@qq.com>
2018-09-04 21:50:40 +0800

    gg

---
+++ src/components/Ride/RideHistoryInfo.js
@@ -0,0 +1,9 @@
+import Taro, { Component } from '@tarojs/taro'
+
+export default class RideHistoryInfo extends Component {
+    render() {
+        return (
+            <View></View>
+        )
+    }
+}
--- src/pages/historyInfo/historyInfo.js
+++ src/pages/historyInfo/historyInfo.js
@@ -2,6 +2,8 @@ import Taro, { Component } from '@tarojs/taro'
 import './historyInfo.styl'
 import MyMap from '../../components/MyMap'
 import createWxCharts from '../../utils/createWxCharts'
+import RunHistoryInfo from '../../components/Run/RunHistoryInfo'
+import RideHistoryInfo from '../../components/Ride/RideHistoryInfo'
 export default class HistoryInfo extends Component {
     config = {
 		navigationBarTitleText: '历史详情'
@@ -19,8 +21,10 @@ export default class HistoryInfo extends Component {
         let renderType = null
         if (this.state.type == 'run') {
             console.log('run')
+            renderType = (<RunHistoryInfo/>)
         } else if (this.state.type == 'ride') {
             console.log('ride')
+            renderType = (<RideHistoryInfo/>)
         } else if (this.state.type == 'climb') {
             console.log('climb')
         } else if (this.state.type == 'playball') {
